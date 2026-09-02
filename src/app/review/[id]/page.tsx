778c2fe9752a3ca7202e2fea704e8c93638ad769
Yous Sibarani <youssibarani17@gmail.com>
2026-09-02 22:44:08 +0700

    feat(review history) add page for review code respond history

--- src/app/review/[id]/page.tsx
+++ src/app/review/[id]/page.tsx
@@ -1,24 +1,51 @@
+"use client";
+
 import { useParams } from "next/navigation";
 import { useReview } from "@/hooks/useReview";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import ReviewSideBar from "@/component/ReviewSideBar";
 
 function page() {
   const params = useParams();
   const id = params.id;
 
-  const [reviewHistory, fetchReviewById] = useReview();
+  const { reviewHistory, historyTitle, fetchReviewById } = useReview();
+
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    if (id) {
+    if (typeof id === "string") {
       fetchReviewById(id);
     }
   }, [id]);
-  return <div>
 
+  const toogleSideBar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
     <div>
-        {reviewHistory.}
+      <ReviewSideBar
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        toogleSideBar={toogleSideBar}
+        historyTitle={historyTitle}
+      />
+
+      <div>{reviewHistory?.language}</div>
+      <div>{reviewHistory?.code}</div>
+      <div>
+        {reviewHistory?.issues.map((issue) => (
+          <div key={issue.id}>
+            <div>{issue.line}</div>
+            <div>{issue.description}</div>
+            <div>{issue.severity}</div>
+            <div>{issue.suggestion}</div>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default page;
